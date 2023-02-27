ef77437a1d3b1aa14368312399dbd617fa0e4205
rictnt <rictnt@gmail.com>
2023-02-27 15:41:20 -0500

    commit 2

---
+++ cap001_intro/e01_servidor_http/helo.js
@@ -0,0 +1,10 @@
+const http = require('http');
+http
+  .createServer((request, response) => {
+    response.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+
+    response.write('hola ...mundo\n');
+    response.end();
+  }).listen(1337);
